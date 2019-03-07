eed6ca7e7f1e91465698fe024d3fa28428b3c155
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-03-07 09:21:30 +0100

    EjAdapterDivX

---
+++ src/adaptadores/DivX.java
@@ -0,0 +1,10 @@
+package adaptadores;
+
+public class DivX implements MediaDivX {
+
+    @Override
+    public void playFilm(String filename) {
+        System.out.println("Playing DivX File " + filename);
+    }
+
+}
